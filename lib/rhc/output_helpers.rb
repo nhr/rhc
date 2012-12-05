2f4ea4c4acb8fe0bac69b779598822fd4308abad
N. Harrison Ripps <nhr@redhat.com>
2012-12-05 10:12:38 -0500

    Fixed output helper to catch nil storage value

--- lib/rhc/output_helpers.rb
+++ lib/rhc/output_helpers.rb
@@ -189,7 +189,7 @@ module RHC
         when :scales_from,:scales_to
           (value == -1 ? "available gears" : value)
         when :base_gear_storage,:additional_gear_storage
-          (value == 0 ? "None" : "#{value}GB")
+          ((value.nil? or value == 0) ? "None" : "#{value}GB")
         when :aliases
           value.join ' '
         else
